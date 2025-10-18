70b6601303c5eea14bc9a7cd0bfa287e99378381
JoaoCintra03 <161538925+JoaoCintra03@users.noreply.github.com>
2025-10-17 22:23:02 -0300

    Add files via upload

---
+++ src/service/token/index.tsx
@@ -0,0 +1,16 @@
+import { jwtDecode } from "jwt-decode"
+
+export const verificaTokenExpirado = (token?: string) => {
+    if (token) {
+        let decodedToken = jwtDecode(token)
+        
+        if (
+            !decodedToken.exp
+            ||
+            decodedToken.exp < new Date().getTime() / 1000
+        ) {
+            return true
+        }
+        return false
+    }
+}
